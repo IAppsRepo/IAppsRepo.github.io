7c667910c569c37d42c55503ffcc167bb3fad618
IAppsRepo <iapps.ipa@gmail.com>
2024-10-26 03:04:45 +0300

    Add files via upload

--- Scripts/Repository.js
+++ Scripts/Repository.js
@@ -3,8 +3,61 @@ const pagination = document.getElementById('pagination');
 const searchInput = document.getElementById('search');
 let apps = [];
 let currentPage = 1;
-const itemsPerPage = 20;
-const maxVisiblePages = 5;
+const maxVisiblePages = 3;
+let displayMode = 'full';
+let itemsPerPage = 20;
+let selectedCategory = '';
+
+document.addEventListener("DOMContentLoaded", function() {
+    const settingsMenu = document.getElementById("settingsMenu");
+    const overlay = document.getElementById("overlay");
+    const settingsBtn = document.getElementById("settingsBtn");
+    const closeBtn = document.getElementById("closeBtn");
+
+    settingsBtn.addEventListener("click", () => {
+        overlay.style.display = 'block';
+        setTimeout(() => {
+            overlay.classList.add("active");
+            settingsMenu.style.display = 'flex';
+            setTimeout(() => {
+                settingsMenu.classList.add('show');
+                settingsMenu.classList.remove('hide');
+            }, 10);
+        }, 10);
+    });
+
+    closeBtn.addEventListener("click", () => {
+        settingsMenu.classList.add('hide');
+        settingsMenu.classList.remove('show');
+        setTimeout(() => {
+            settingsMenu.style.display = 'none';
+            overlay.classList.remove("active");
+            overlay.style.display = 'none';
+        }, 10);
+    });
+});
+
+
+document.getElementById('settingsBtn').addEventListener('click', function () {
+    const settingsMenu = document.getElementById('settingsMenu');
+    settingsMenu.classList.toggle('active');
+});
+
+document.querySelectorAll('input[name="categoryApps"]').forEach(radio => {
+    radio.addEventListener('change', function () {
+        selectedCategory = this.value;
+        currentPage = 1;
+        displayApps();
+    });
+});
+
+document.querySelectorAll('input[name="itemsPerPage"]').forEach(radio => {
+    radio.addEventListener('change', function () {
+        itemsPerPage = parseInt(this.value, 10);
+        currentPage = 1;
+        displayApps();
+    });
+});
 
 fetch('Repo.json')
     .then(response => response.json())
@@ -14,39 +67,79 @@ fetch('Repo.json')
         setupPagination();
     });
 
+document.getElementById('full-list-btn').addEventListener('click', () => {
+    displayMode = 'full';
+    currentPage = 1;
+    displayApps();
+    
+    document.querySelector('.full-button').classList.add('active');
+    document.querySelector('.time-button').classList.remove('active');
+});
+
+document.getElementById('time-list-btn').addEventListener('click', () => {
+    displayMode = 'time';
+    currentPage = 1;
+    displayApps();
+
+    document.querySelector('.time-button').classList.add('active');
+    document.querySelector('.full-button').classList.remove('active');
+});
+
 function displayApps() {
     appList.innerHTML = '';
-    const filteredApps = apps.filter(app => app.appName.toLowerCase().includes(searchInput.value.toLowerCase()));
+
+    let filteredApps = apps.filter(app => app.appName.toLowerCase().includes(searchInput.value.toLowerCase()));
+
+    if (selectedCategory) {
+        filteredApps = filteredApps.filter(app => app.appName.toLowerCase().includes(`#${selectedCategory.toLowerCase()}`));
+    }
+
+    if (displayMode === 'time') {
+        filteredApps.sort((a, b) => new Date(b.appUpdateTime) - new Date(a.appUpdateTime));
+    }
+
     const start = (currentPage - 1) * itemsPerPage;
     const end = start + itemsPerPage;
-    filteredApps.slice(start, end).forEach(app => {
+    filteredApps.slice(start, end).forEach((app, index) => {
         const formattedDescription = app.appDescription.replace(/\n/g, '<br>');
-        appList.innerHTML += `
-            <div class="app-item">
-                <div class="app-header">
-                    <img src="${app.appImage}" alt="${app.appName}" style="width: 50px;">
-                    <div>
-                        <h3>${app.appName}</h3>
-                        <p>${app.appVersion}</p>
-                    </div>
+        const delay = index * 0.15;
+
+        const cleanedAppName = app.appName.split('\n')[0];
+
+        const appItem = document.createElement('div');
+        appItem.className = 'app-item';
+        appItem.style.animationDelay = `${delay}s`;
+        appItem.innerHTML = `
+            <div class="app-header">
+                <img src="${app.appImage}" alt="${cleanedAppName}" style="width: 50px;">
+                <div>
+                    <h3>${cleanedAppName}</h3>
+                    <p>${app.appVersion}</p>
                 </div>
-                <p class="app-description">${formattedDescription}</p>
             </div>
+            <hr>
+            <p class="app-description">${formattedDescription}</p>
         `;
+        appList.appendChild(appItem);
     });
+
     setupPagination(filteredApps);
 }
 
 function setupPagination(filteredApps = apps) {
     pagination.innerHTML = '';
     const totalPages = Math.ceil(filteredApps.length / itemsPerPage);
-    
+
     function createPageButton(page) {
         const pageBtn = document.createElement('button');
         pageBtn.innerText = page;
         pageBtn.addEventListener('click', () => {
             currentPage = page;
             displayApps();
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });
         });
         if (currentPage === page) {
             pageBtn.disabled = true;
@@ -60,6 +153,10 @@ function setupPagination(filteredApps = apps) {
         firstPageBtn.addEventListener('click', () => {
             currentPage = 1;
             displayApps();
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });
         });
         pagination.appendChild(firstPageBtn);
     }
@@ -70,6 +167,10 @@ function setupPagination(filteredApps = apps) {
         prevPageBtn.addEventListener('click', () => {
             currentPage--;
             displayApps();
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });
         });
         pagination.appendChild(prevPageBtn);
     }
@@ -87,6 +188,10 @@ function setupPagination(filteredApps = apps) {
         nextPageBtn.addEventListener('click', () => {
             currentPage++;
             displayApps();
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });
         });
         pagination.appendChild(nextPageBtn);
     }
@@ -97,6 +202,10 @@ function setupPagination(filteredApps = apps) {
         lastPageBtn.addEventListener('click', () => {
             currentPage = totalPages;
             displayApps();
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });
         });
         pagination.appendChild(lastPageBtn);
     }
@@ -105,4 +214,8 @@ function setupPagination(filteredApps = apps) {
 searchInput.addEventListener('input', () => {
     currentPage = 1;
     displayApps();
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
 });
